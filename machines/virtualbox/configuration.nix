eea904d1d0ec43054d2eff0c3fbe0c923bd6d1f3
Jonas Wouters <jonas@jw.be>
2018-12-01 23:39:27 +0100

    Create configuration for virtualbox

---
+++ machines/virtualbox/configuration.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./users.nix
+    ];
+    
+  # Boot settings
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "/dev/sda";
+  boot.initrd.checkJournalingFS = false;
+  
+  # This value determines the NixOS release with which your
+  # system is to be compatible, in order to avoid breaking
+  # some software such as database servers. You should
+  # change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "18.09";
+  
+  # Networking
+  networking = {
+    hostName = "virtualbox";
+  };
