7572ee101af2d7e1097e8d51dcc30057ffd0792e
Jonas Wouters <jonas@jw.be>
2018-12-02 22:38:25 +0100

    Create i3.nix

---
+++ packages/user/desktop/i3.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }:
+
+{
+  home-manager.users.jonaswouters = {
+     xsession = {
+      enable = true;
+      
+      windowManager.i3 = rec {
+        enable = true;
+      }
+    };
+  };
+}
