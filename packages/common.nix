c4f1c26a402940cdc9d1294e6a8ebc77aa050910
Jonas Wouters <jonas@jw.be>
2018-12-02 14:26:12 +0100

    Add common packages

---
+++ packages/common.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    # Editors
+    (neovim.override { vimAlias = true; })
+
+    # Development
+    gitAndTools.tig
+    gitAndTools.git
+
+    # Admin
+    htop
+    pciutils
+    tree
+    wget
+    curl
+  ];
+}
