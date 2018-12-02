95972dde9807dbfac15601503f29807064fca841
Jonas Wouters <jonas@jw.be>
2018-12-02 22:34:18 +0100

    Update base.nix

--- machines/base.nix
+++ machines/base.nix
@@ -6,7 +6,7 @@
     [ # Include the results of the hardware scan.
       ../hardware-configuration.nix
       ../users.nix
-      ../packages/system-common.nix
+      ../packages/common.nix
     ];
     
     # Alow unfree packages
