d9405615d83961fd165ea42cbf0303bd2a92f93d
Jonas Wouters <jonas@jw.be>
2018-12-04 15:21:42 +0100

    Update users.nix

--- users.nix
+++ users.nix
@@ -1,5 +1,11 @@
 { config, lib, pkgs, ... }:
 
+let
+  sshKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDKBFYgkQJm64CKzv5lfzgWA8oRAaVSHQkPQBR/nk8TkykvEfJYRWcIvsiYjfJpJqdidsWQqbl9WxBL9raF6fZZ9XNIffofo+gyH+xn+m9haPyynSJbbvg47J4VJgKGF/8xGGOlaFERKaZLToPYhwq15jIxu5etwd+AbX/liCB1UIjpggvZ+XonGj0ULIPN9yA3kRaiv4NuLXSFlz2uIpluF+4PW5N8bHFmHS4D1w2hLHuJWBp73Puo9E7hpjn8HWB7QAklmruYFSgiyduhLIakAtIkWWd2SPfnilAUCqTmHJZuVK7oG5LJ1vr7k1OVQFwfleStPrqqlbRXeemf66xX jonas@jw.be";
+  username = "jonaswouters";
+  email = "jonas@jw.be";
+  fullName = "Jonas Wouters"
+in
 {
   imports = [
     # Home manager import
@@ -7,11 +13,11 @@
   ];
 
   # Users
-  users.users.jonaswouters = {
+  users.users.${username} = {
     isNormalUser = true;
     uid = 1000;
     createHome = true;
-    description = "Jonas Wouters";
+    description = "${fullName}";
     extraGroups = [
       "audio"
       "docker"
@@ -19,13 +25,16 @@
       "video"
       "wheel"
     ];
+    openssh.authorizedKeys.keys = [
+      sshKey
+    ];
   };
   
   # Home manager
-  home-manager.users.jonaswouters = { 
+  home-manager.users.${username} = { 
     programs.git = {
       enable = true;
-      userName  = "Jonas Wouters";
+      userName  = "${fullName}";
       userEmail = "jonas@jw.be";
     };
     
