6a4239aac8d442b4392f0b0cc6535a947d98a09f
Jonas Wouters <jonas@jw.be>
2018-12-04 15:35:27 +0100

    Update users.nix

--- users.nix
+++ users.nix
@@ -4,7 +4,7 @@ let
   sshKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDKBFYgkQJm64CKzv5lfzgWA8oRAaVSHQkPQBR/nk8TkykvEfJYRWcIvsiYjfJpJqdidsWQqbl9WxBL9raF6fZZ9XNIffofo+gyH+xn+m9haPyynSJbbvg47J4VJgKGF/8xGGOlaFERKaZLToPYhwq15jIxu5etwd+AbX/liCB1UIjpggvZ+XonGj0ULIPN9yA3kRaiv4NuLXSFlz2uIpluF+4PW5N8bHFmHS4D1w2hLHuJWBp73Puo9E7hpjn8HWB7QAklmruYFSgiyduhLIakAtIkWWd2SPfnilAUCqTmHJZuVK7oG5LJ1vr7k1OVQFwfleStPrqqlbRXeemf66xX jonas@jw.be";
   username = "jonaswouters";
   email = "jonas@jw.be";
-  fullName = "Jonas Wouters"
+  fullName = "Jonas Wouters";
 in
 {
   imports = [
