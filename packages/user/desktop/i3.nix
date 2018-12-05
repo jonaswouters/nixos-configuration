787a5ee98e70fe3c41580bca30b412d1d92ed5d2
Jonas Wouters <jonas@jw.be>
2018-12-05 20:52:16 +0000

    Scale i3 on laptop for now

--- packages/user/desktop/i3.nix
+++ packages/user/desktop/i3.nix
@@ -1,9 +1,10 @@
-{ config, pkgs, lib, ... }:
+  { config, pkgs, lib, ... }:
 
 let
   modifier = "Mod4";
   move = "50px";
   terminal = "termite";
+  dpi = "150";
 in
 {
   imports =
@@ -22,7 +23,11 @@ in
         package = pkgs.i3-gaps; 
         config = {
           modifier = "${modifier}";
-                  
+  
+          startup = [
+            { command = "xrandr --dpi ${dpi}"; }
+          ];
+
           keybindings = lib.mkOptionDefault {
             "${modifier}+Return" = "exec ${terminal}";
             "${modifier}+d" = "exec rofi -show drun -show-icons -display-drun \"Launcher\"";
