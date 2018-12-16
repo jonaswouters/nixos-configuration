cfbad0a5fe0ad76d20809430a3e98a86112095ce
Jonas Wouters <jonas@jw.be>
2018-12-16 21:59:48 +0100

    Update annabel.nix

--- machines/linode/annabel.nix
+++ machines/linode/annabel.nix
@@ -15,8 +15,8 @@
   boot.loader.grub.extraConfig = "serial; terminal_input serial; terminal_output serial";
   boot.kernelParams = [ "console=ttyS0" ];
   
-  // Fix error: unable to fork: Cannot allocate memory
-  // https://github.com/NixOS/nix/issues/421
+  # Fix error: unable to fork: Cannot allocate memory
+  # https://github.com/NixOS/nix/issues/421
   boot.kernel.sysctl."vm.overcommit_memory" = "1";
 
   services.longview = {
