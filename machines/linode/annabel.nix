3120431a23c795704931b9d80d26a58a2251681d
Jonas Wouters <jonas@jw.be>
2018-12-16 21:59:35 +0100

    Create annabel.nix

---
+++ machines/linode/annabel.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.copyKernels = true;
+  boot.loader.grub.fsIdentifier = "label";
+  boot.loader.grub.extraConfig = "serial; terminal_input serial; terminal_output serial";
+  boot.kernelParams = [ "console=ttyS0" ];
+  
+  // Fix error: unable to fork: Cannot allocate memory
+  // https://github.com/NixOS/nix/issues/421
+  boot.kernel.sysctl."vm.overcommit_memory" = "1";
+
+  services.longview = {
+    enable = true;
+    apiKey = "";
+
+    apacheStatusUrl = "";
+    nginxStatusUrl = "";
+
+    mysqlUser = "";
+    mysqlPassword = "";
+  };
+
+  # Enable the OpenSSH server.
+  services.openssh.enable = true;
+  services.openssh.permitRootLogin = "yes";
