faa0bed111b585b8395d21d7f963fdf9b046a457
Jonas Wouters <jonas@jw.be>
2018-12-17 11:29:23 +0100

    Update base.nix

--- machines/base.nix
+++ machines/base.nix
@@ -4,16 +4,12 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ../hardware-configuration.nix
       ../users.nix
       ../packages/common.nix
     ];
     
     # Alow unfree packages
     nixpkgs.config.allowUnfree = true;
-    
-    # Enable exfat for mounted drives
-    boot.extraModulePackages = [ config.boot.kernelPackages.exfat-nofuse ];
     
     # Enable OpenSSH
     services.openssh = {
