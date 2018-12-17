3e7b872d6a056337e80cb5a5a7aa12551501c3e9
Jonas Wouters <jonas@jw.be>
2018-12-17 11:29:57 +0100

    Update base-workstation.nix

--- machines/base-workstation.nix
+++ machines/base-workstation.nix
@@ -5,8 +5,6 @@
     [ # Include the results of the hardware scan.
       ../base-unstable.nix
       ../hardware-configuration.nix
-      ../users.nix
-      ../packages/common.nix
     ];
     
     # Enable exfat for mounted drives
