6083a7e684595726081a1457ba5a9ed3e37af9fe
Jonas Wouters <jonas@jw.be>
2018-12-19 11:50:42 +0100

    Update znc.nix

--- packages/znc.nix
+++ packages/znc.nix
@@ -4,22 +4,23 @@
   services.znc = {
     enable = true;
     useLegacyConfig = false;
-    config = 
-    {
-      LoadModule = [ "webadmin" "adminlog" ];
-      User.paul = {
-        Admin = true;
-        Nick = "jonaswouters";
-        AltNick = "zertox";
-        LoadModule = [ "chansaver" "controlpanel" ];
-        Network.freenode = {
-          Server = "chat.freenode.net +6697";
-          LoadModule = [ "simple_away" ];
-          Chan = {
-            "#nixos" = { Detached = false; };
-          };
-        };
-      };
-    };
+    ## Enabled in next version 
+    # config = 
+    # {
+    #   LoadModule = [ "webadmin" "adminlog" ];
+    #   User.paul = {
+    #     Admin = true;
+    #     Nick = "jonaswouters";
+    #     AltNick = "zertox";
+    #     LoadModule = [ "chansaver" "controlpanel" ];
+    #     Network.freenode = {
+    #       Server = "chat.freenode.net +6697";
+    #       LoadModule = [ "simple_away" ];
+    #       Chan = {
+    #         "#nixos" = { Detached = false; };
+    #       };
+    #     };
+    #   };
+    # };
   };
 }
