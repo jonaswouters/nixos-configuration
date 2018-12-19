aa93546072f19ac671e880c870a0cf22e1f1fbfe
Jonas Wouters <jonas@jw.be>
2018-12-19 12:15:09 +0100

    Update znc.nix

--- packages/znc.nix
+++ packages/znc.nix
@@ -4,9 +4,30 @@
   services.znc = {
     enable = true;
     
-    # passBlock with `nix-shell -p znc --command "znc --makepass"`.
-    # and place it inside secrets.nix
-    passBlock = (import ../secrets.nix).zncPassBlock;
+    openFirewall = true;
+    confOptions = {
+      networks = {
+        freenode = {
+          channels = [
+            "nixos"
+          ];
+          modules = [ "log" "simple_away" ];
+          server = "chat.freenode.net";
+          port = 6697;
+          useSSL = true;
+          userName = "jonaswouters";
+          password = (import ../secrets.nix).ircPassword;
+        };
+      };
+      
+      modules = [ "adminlog" "log" ];
+      nick = "jonaswouters";
+    
+      # passBlock with `nix-shell -p znc --command "znc --makepass"`.
+      # and place it inside secrets.nix
+      passBlock = (import ../secrets.nix).zncPassBlock;
+      
+    };
     
     ## Enabled in next version 
     # useLegacyConfig = false;
