e45ed8ecec368a2e5aa770ee0f97c642323297d5
Jonas Wouters <jonas@jw.be>
2018-12-19 11:25:16 +0100

    Create znc.nix

---
+++ packages/znc.nix
@@ -0,0 +1,23 @@
+{ pkgs, ... }:
+
+{
+  services.znc.enable = true;
+  services.znc.useLegacyConfig = false;
+  services.znc.config = 
+  {
+    LoadModule = [ "webadmin" "adminlog" ];
+    User.paul = {
+      Admin = true;
+      Nick = "jonaswouters
+      AltNick = "zertox
+      LoadModule = [ "chansaver" "controlpanel" ];
+      Network.freenode = {
+        Server = "chat.freenode.net +6697";
+        LoadModule = [ "simple_away" ];
+        Chan = {
+          "#nixos" = { Detached = false; };
+        };
+      };
+    };
+  }
+}
