da2b1f1f0eb677393dd4b80d00b2548b62406451
Jonas Wouters <jonas@jw.be>
2018-12-25 10:37:34 +0000

    Add controller support

--- packages/user/desktop/programs/games.nix
+++ packages/user/desktop/programs/games.nix
@@ -5,6 +5,18 @@ in
 {
   home-manager.users.${secrets.username}.home.packages = with pkgs; [
     steam
+
   ];
 
+  environment.systemPackages = with pkgs; [
+    # Controller drivers
+    xboxdrv
+    steamcontroller
+  ];
+
+# Allow steam controller
+services.udev.extraRules = ''
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="28de", MODE="0666"
+    KERNEL=="uinput", MODE="0660", GROUP="users", OPTIONS+="static_node=uinput"
+  '';
 }
