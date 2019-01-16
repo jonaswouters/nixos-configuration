7386a5562c1fcd1768c59d7addb7e5c1a9a5a7c5
Jonas Wouters <jonas@jw.be>
2019-01-16 10:01:57 +0100

    Add 20 workspaces in i3

--- packages/user/desktop/i3.nix
+++ packages/user/desktop/i3.nix
@@ -75,6 +75,17 @@ in
             "${modifier}+7" = "workspace 7";
             "${modifier}+8" = "workspace 8";
             "${modifier}+9" = "workspace 9";
+            "${modifier}+0" = "workspace 10";
+            "${modifier}+Ctrl+1" = "workspace 11";
+            "${modifier}+Ctrl+2" = "workspace 12";
+            "${modifier}+Ctrl+3" = "workspace 13";
+            "${modifier}+Ctrl+4" = "workspace 14";
+            "${modifier}+Ctrl+5" = "workspace 15";
+            "${modifier}+Ctrl+6" = "workspace 16";
+            "${modifier}+Ctrl+7" = "workspace 17";
+            "${modifier}+Ctrl+8" = "workspace 18";
+            "${modifier}+Ctrl+9" = "workspace 19";
+            "${modifier}+Ctrl+0" = "workspace 20";
 
             "${modifier}+Shift+1" = "move container to workspace 1";
             "${modifier}+Shift+2" = "move container to workspace 2";
@@ -85,6 +96,17 @@ in
             "${modifier}+Shift+7" = "move container to workspace 7";
             "${modifier}+Shift+8" = "move container to workspace 8";
             "${modifier}+Shift+9" = "move container to workspace 9";
+            "${modifier}+Shift+0" = "move container to workspace 10";
+            "${modifier}+Ctrl+Shift+1" = "move container to workspace 11";
+            "${modifier}+Ctrl+Shift+2" = "move container to workspace 12";
+            "${modifier}+Ctrl+Shift+3" = "move container to workspace 13";
+            "${modifier}+Ctrl+Shift+4" = "move container to workspace 14";
+            "${modifier}+Ctrl+Shift+5" = "move container to workspace 15";
+            "${modifier}+Ctrl+Shift+6" = "move container to workspace 16";
+            "${modifier}+Ctrl+Shift+7" = "move container to workspace 17";
+            "${modifier}+Ctrl+Shift+8" = "move container to workspace 18";
+            "${modifier}+Ctrl+Shift+9" = "move container to workspace 19";
+            "${modifier}+Ctrl+Shift+0" = "move container to workspace 20";
 
             "${modifier}+Shift+c" = "reload";
             "${modifier}+Shift+r" = "restart";
