eeebb275bbe99844c017ac9190d9ca76943984ba
Jonas Wouters <jonas@jw.be>
2019-03-21 10:57:02 +0100

    Add alias to clean nix env

--- packages/user/fish/alias.fish
+++ packages/user/fish/alias.fish
@@ -1,6 +1,7 @@
 alias tig "tig --show-signature"
 alias vim nvim
 alias vi nvim
+alias nix-cleanup "sudo nix-env --delete-generations old && nix-collect-garbage -d"
 
 function delline
   sed -i "$argv[2]"d $argv[1] 
